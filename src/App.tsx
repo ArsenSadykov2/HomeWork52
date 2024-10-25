f84309220e834c9e9dbfbedfca3139d5d268bf1f
ArsenSadykov <LoCo10291998@gmail.com>
2024-10-26 02:33:38 +0600

    Third level is done. made it so that when you click on a button, a copy of the deck is displayed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import './App.css'
 import Card from "./Components/Card/Card.tsx";
+import CardDeck from "./lib/CardDeck.ts";
+import {useState} from "react";
+
+const cardDeck = new CardDeck();
 
 
 const App = () => {
-
+  const [hand, setHand] = useState<Card[]>([]);
+  const getFiveCards = () => {
+      setHand(cardDeck.getCards(5));
+  };
 
   return (
       <>
+        <h4>Cards: {cardDeck.deck.length}</h4>
+        <hr/>
+        {cardDeck.deck.length < 5 ?
+            null
+            :
+            <button onClick={getFiveCards}>Get Cards</button>}
+        <hr/>
         <div className="playingCards faceImages">
-          <Card rank="7" suit="diams"/>
+          {hand.map((card) => (
+              <Card key={card.rank + card.suit} rank={card.rank} suit={card.suit}
+              />
+          ))}
         </div>
 
       </>
